33f6f03670fd17e3a7ea2e474837357e53d311c4
Mario <mcalvarr@ucm.es>
2023-10-20 13:46:42 +0200

    3.4.1

--- 3/ejercicio4/espacio.c
+++ 3/ejercicio4/espacio.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 /* Forward declaration */
 int get_size_dir(char *fname, size_t *blocks);
@@ -9,7 +12,13 @@ int get_size_dir(char *fname, size_t *blocks);
  */
 int get_size(char *fname, size_t *blocks)
 {
-
+    struct stat st;
+    lstat(fname, &st);
+    if (S_ISDIR(st.st_mode)) {
+        get_size_dir(fname, blocks);
+    }
+    *blocks += st.st_blocks;
+    return 0;
 }
 
 
@@ -19,7 +28,21 @@ int get_size(char *fname, size_t *blocks)
  */
 int get_size_dir(char *dname, size_t *blocks)
 {
-
+    DIR* dir;
+    struct dirent* entry;
+    dir = opendir(dname);
+    char fname[512];
+    while ((entry = readdir(dir)) != NULL) {
+        if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, "..")) {
+            continue; 
+        }
+        sprintf(fname, "%s/%s", dname, entry->d_name);
+        size_t aux = 0;
+        get_size(fname, &aux);
+        *blocks += aux;
+    }
+    closedir(dir);
+    return 0;
 }
 
 /* Processes all the files in the command line calling get_size on them to
@@ -28,6 +51,16 @@ int get_size_dir(char *dname, size_t *blocks)
  */
 int main(int argc, char *argv[])
 {
+    if (argc <= 1) {
+        perror("Incorrect number of arguments (1 or more are requiered).");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t blocks;
+    for (int i = 1; i < argc; i++) {
+        get_size(argv[i], &blocks);
+        printf("%luK\t%s\n", blocks/2, argv[i]);
+    }
 
 	return 0;
 }
