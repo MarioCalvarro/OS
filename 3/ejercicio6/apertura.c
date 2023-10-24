df6958c628aa3d500370996b5c48752e141ee0b4
Mario <mcalvarr@ucm.es>
2023-10-24 17:06:31 +0200

    3.6.2

--- 3/ejercicio6/apertura.c
+++ 3/ejercicio6/apertura.c
@@ -1,7 +1,22 @@
+/**
+ * 1. Hemos otorgado permiso de lectura/escritura al usuario dueño, por tanto,
+ *    es el único que puede hacerlo. Da error cuando intentamos leer teniendo 
+ *    solo permisos de escritura o cuando intentamos leer o escribir teniendo 
+ *    solo permisos de lectura porque no podemos leer nada al estar vacío el 
+ *    fichero (al abrirlo lo vaciamos y no tenemos permiso de escritura para 
+ *    rellenarlo).
+ *
+ * 2. Da error el solo lectura y el escritura/lectura(no hay permisos)
+ *
+ * 3. No deja ejecutar el programa al usuario dueño del archivo.
+ *
+ */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #include "defs.h"
 
@@ -14,19 +29,19 @@ int open_file(struct options options)
         perror("No read or write option selected.");
         exit(EXIT_FAILURE);
     } else if (options.write_file == 0) {
-        fdo = open(options.input_file, O_RDONLY | O_CREAT | O_TRUNC);
+        fdo = open(options.input_file, O_RDONLY | O_CREAT | O_TRUNC, S_IRUSR);
     } else if (options.read_file == 0) {
-        fdo = open(options.input_file, O_WRONLY | O_CREAT | O_TRUNC);
+        fdo = open(options.input_file, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR);
     } else {
-        fdo = open(options.input_file, O_RDWR | O_CREAT | O_TRUNC);
+        fdo = open(options.input_file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     }
 
+    printf("ERROR: %d\n", errno);
     return fdo;
 }
 
 int main(int argc, char *argv[]) {
     char opt;
-    printf("Hola");
 
     struct options options;
 
@@ -65,6 +80,7 @@ int main(int argc, char *argv[]) {
     }
 
     char buf[NUMBER_CHARS];
+    lseek(fdo, 0, SEEK_SET);
     if (read(fdo, buf, NUMBER_CHARS) < 1) {
         perror("Could not read file.");
     }
